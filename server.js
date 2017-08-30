bdf91e903c9943c03276a21d2e868dd6d22fa976
Terri Pannell <terripannell@gmail.com>
2017-08-30 14:25:20 -0400

    Adding heroku support

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var path = require("path");
 
-var port = 3000;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
@@ -26,4 +26,6 @@ var routes = require("./controllers/burgers_Controller.js");
 
 app.use("/", routes);
 
-app.listen(port);
+app.listen(PORT, function(){
+    console.log("app is listening on PORT: " + PORT);
+});
