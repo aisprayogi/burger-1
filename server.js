1c803e0b437b6c540b36550a900826b09badea06
Terri Pannell <terripannell@gmail.com>
2017-09-05 15:11:51 -0400

    changed name of burgers_controller.js file name in require statement

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var path = require("path");
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3306;
 
 var app = express();
 
@@ -23,7 +23,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgers_Controller.js");
+var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
