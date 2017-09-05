0da4bb4a0a730bf7aa7a17f5b061ce9decc6a881
Terri Pannell <terripannell@gmail.com>
2017-09-05 13:43:27 -0400

    trying to fix heroku problems

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ var PORT = process.env.PORT || 3000;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+//app.use(express.static("public"));
+app.use("/static/", express.static(path.join(__dirname, "public")));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
